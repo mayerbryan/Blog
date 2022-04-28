6c25f19dc67322da10005a54c5e5ce66290fad92
Bryan Mayer <mayerbryan@mail.com>
2022-04-28 09:59:52 -0300

    update

--- Program.cs
+++ Program.cs
@@ -10,56 +10,57 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+            var user = new User
             {
-                var tag = new Tag { Name = ".NET", Slug = "dotnet" };
-                context.Tags.Add(tag);
-                context.SaveChanges();
+                Name = "André Baltieri",
+                Slug = "andrebaltieri",
+                Email = "andre@balta.io",
+                Bio = "9x Microsoft MVP",
+                Image = "https://balta.io",
+                PasswordHash = "123098457"
+            };
+            
+            var category = new Category {Name = "Backend", Slug = "backend"};
+            
+            var post = new Post
+            {
+                Author = user,
+                Category = category,
+                Body = "<p>Hello world</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF core",
+                Title = "Começando com EF Core",
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now,
+            };
+            
+            context.Posts.Add(post); // Não adicionei autor nem categoria
+            context.SaveChanges();
 
-                var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet" };
-                context.Tags.Add(tag2);
-                context.SaveChanges();
-
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
-                tag.Name = ".NET";
-                tag.Slug = "dotnet";
-
-                context.Update(tag);
-                context.SaveChanges();
-
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
-
-                context.Remove(tag);
-                context.SaveChanges();
-
-                var tags = context
-                    .Tags
-                    .AsNoTracking()
-                    .ToList();
-
-                foreach (var tag in tags)
-                {
-                    Console.WriteLine(tag.Name);
-                }
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking() // REMOVER
-                    .FirstOrDefault(x => x.Id == 3);
-
-                tag.Name = "Ponto NET";
-                tag.Slug = "dotnet";
-
-                context.Update(tag);
-                context.SaveChanges();
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking()
-                    .FirstOrDefault(x => x.Id == 3);
-
-                Console.WriteLine(tag?.Name);
-            }
+            var posts = context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderBy(x => x.LastUpdateDate)
+                .ToList();
+            
+            foreach (var post in posts) 
+                Console.WriteLine($"{post.Title} por {post.Author.Name} em {post.Category.Name}");
+            
+            var post = context
+                .Posts
+                // .AsNoTracking() PRECISA DO TRACKING 
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderBy(x => x.LastUpdateDate)
+                .FirstOrDefault(); // Pegando o primeiro item
+            
+            post.Author.Name = "Uncle Bob";
+            
+            context.Posts.Update(post);
+            context.SaveChanges();
         }
     }
 }
